95b0c07e64a82dcf05b84d8adb77adc281b6b578
Kevin Williams <kevin.williams@kevinwilliams.dev>
2025-02-18 16:38:55 -0600

    Add interactive IdentityServer pages

--- Agrigate.Identity/Config.cs
+++ Agrigate.Identity/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Agrigate.Identity;
 
@@ -21,7 +22,8 @@ public static class Config
     
     public static IEnumerable<IdentityResource> IdentityResources =>
     [
-        new IdentityResources.OpenId()
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
     ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -38,6 +40,20 @@ public static class Config
             AllowedGrantTypes = { GrantType.ClientCredentials },
             ClientSecrets = { new Secret("secret".Sha256()) },
             AllowedScopes = { AgrigateApi }
+        },
+        // Interactive .Net Core Web App
+        new Client
+        {
+            ClientId = AgrigateWeb,
+            AllowedGrantTypes = GrantTypes.Code,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            RedirectUris = { "http://localhost:5003/signin-oidc" },
+            PostLogoutRedirectUris = { "http://localhost:5003/signout-callback-oidc" },
+            AllowedScopes =
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+            }
         }
     ];
 }
