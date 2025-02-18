be250efd55dd38013ccf1f1f1a685ea0790c9b9d
Kevin Williams <kevin.williams@kevinwilliams.dev>
2025-02-17 18:50:22 -0600

    Added Identity Server

---
+++ Agrigate.Identity/Config.cs
@@ -0,0 +1,38 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Agrigate.Identity;
+
+public static class Config
+{
+    /// <summary>
+    /// The API that powers Agrigate
+    /// </summary>
+    private const string AgrigateApi = "Agrigate.Api";
+    
+    /// <summary>
+    /// Agrigate's user-facing web application
+    /// </summary>
+    private const string AgrigateWeb = "Agrigate.Web";
+    
+    public static IEnumerable<IdentityResource> IdentityResources =>
+    [
+        new IdentityResources.OpenId()
+    ];
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+    [
+        new ApiScope(name: AgrigateApi, displayName: "Agrigate API")
+    ];
+
+    public static IEnumerable<Client> Clients =>
+    [
+        // Machine to Machine Client
+        new Client
+        {
+            ClientId = AgrigateWeb,
+            AllowedGrantTypes = { GrantType.ClientCredentials },
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedScopes = { AgrigateApi }
+        }
+    ];
+}
